842a9d50c09ab57cdcb9c704b938f5a976411026
Robert Mazza <robertmazza1@gmail.com>
2016-09-24 13:59:46 -0500

    changed controller variable for easier use

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,18 +2,17 @@ angular.module('weather-app')
     .controller('searchController', ['$scope', '$log', 'weatherService', function ($scope, $log, weatherService) {
 
         $scope.weatherData = {};
+        $scope.displayLocation = {};
         $scope.searchValue = '';
-        $scope.city = '';
-        $scope.state = '';
-
+        
         $scope.getSearch = function (city) {
 
             weatherService.getToday(city)
                 .then(function (response) {
 
                     $scope.weatherData = response.data;
-                    $scope.city = response.data.current_observation.display_location.city;
-                    $scope.state = response.data.current_observation.display_location.state;
+                    $scope.displayLocation = response.data.current_observation.display_location;
+
 
 
                     $scope.searchValue = '';
